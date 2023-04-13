60202344ca1b15e7b84e9fbb1ba25f78f649d17f
AmeyPank <pankantiameya@.com>
2023-04-13 13:42:28 +0530

    Banana

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function removeColor() {
+    const selectElement = document.getElementById("colorSelect");
+    const selectedIndex = selectElement.selectedIndex;
+    if (selectedIndex !== -1) {
+      selectElement.remove(selectedIndex);
+    }
+  }
+  
+  const removeButton = document.querySelector('input[type="button"][value="Select and Remove"]');
+  removeButton.addEventListener("click", removeColor);
+  
